2bc57bac1b58822845849372d7d7752f573fe0e9
elenipage <elenipage@icloud.com>
2024-11-18 16:15:38 +0000

    Add xp context to allow the xp to be dynamic in the header

--- App.js
+++ App.js
@@ -3,16 +3,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { PaperProvider } from "react-native-paper";
 import RootStack from "./src/navigation/Stack";
-
-
+import { XpProvider } from "./src/context/Xp-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <PaperProvider>
-        <NavigationContainer>
-          <RootStack/>
-        </NavigationContainer>
+        <XpProvider>
+          <NavigationContainer>
+            <RootStack />
+          </NavigationContainer>
+        </XpProvider>
       </PaperProvider>
     </SafeAreaProvider>
   );
--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -10,13 +10,15 @@ import { ItemConfirmation } from "../pages/ItemConfirmation";
 import { AddNewItem } from "../pages/AddNewItem";
 import Tabs from "./Tabs";
 import { Appbar, Tooltip, Avatar } from "react-native-paper";
+import { useXp } from "../context/Xp-context";
 
 const Stack = createStackNavigator();
 
 function HeaderDemo({ navigation, route, options, back }) {
+  const { xp } = useXp();
 
   return (
-    <Appbar.Header style={{justifyContent: "space-between"}}>
+    <Appbar.Header style={{ justifyContent: "space-between" }}>
       {back ? (
         <Appbar.BackAction onPress={navigation.goBack} />
       ) : (
@@ -33,82 +35,68 @@ function HeaderDemo({ navigation, route, options, back }) {
           />
         </TouchableOpacity>
       )}
-      {back ? (<Appbar.Content title={`${route.name}`}/>) : null}
-      <View style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center"}}>
-      <Tooltip title="Streak">
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Appbar.Action icon="fire-circle" onPress={() => {}}></Appbar.Action>
-          <Text>0</Text>
-        </View>
-      </Tooltip>
-      <Tooltip title="XP">
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Appbar.Action icon="one-up" onPress={() => {}} />
-          {/* <Text>{`${xp} XP`}</Text> */}
-        </View>
-      </Tooltip>
+      {back ? <Appbar.Content title={`${route.name}`} /> : null}
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Tooltip title="Streak">
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Appbar.Action
+              icon="fire-circle"
+              onPress={() => {}}
+            ></Appbar.Action>
+            <Text>0</Text>
+          </View>
+        </Tooltip>
+        <Tooltip title="XP">
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Appbar.Action icon="one-up" onPress={() => {}} />
+            <Text>{`${xp} XP`}</Text>
+          </View>
+        </Tooltip>
       </View>
-      
     </Appbar.Header>
   );
 }
 
 function RootStack() {
-  
+  const { xp, setXp } = useXp();
   const navigation = useNavigation();
   return (
     <Stack.Navigator
       initialRouteName="Main"
       headerMode="screen"
       screenOptions={{
-        header: (props) => <HeaderDemo {...props}/>,
+        header: (props) => <HeaderDemo {...props} />,
       }}
     >
-      <Stack.Screen
-        name="Main"
-        component={Tabs}
-        initialParams={xp}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={Profile}
-      />
+      <Stack.Screen name="Main" component={Tabs} />
+      <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen
         name="Quiz"
         component={Quiz}
+        initialParams={{ xp: xp, setXp: setXp }}
       />
-      <Stack.Screen
-        name="FAQ"
-        component={FAQ}
-      />
-      <Stack.Screen
-        name="Info"
-        component={Info}
-      />
-      <Stack.Screen
-        name="Item Confirmation"
-        component={ItemConfirmation}
-
-      />
-      <Stack.Screen
-        name="Add a New Item"
-        component={AddNewItem}
-      />
+      <Stack.Screen name="FAQ" component={FAQ} />
+      <Stack.Screen name="Info" component={Info} />
+      <Stack.Screen name="Item Confirmation" component={ItemConfirmation} />
+      <Stack.Screen name="Add a New Item" component={AddNewItem} />
     </Stack.Navigator>
   );
 }
