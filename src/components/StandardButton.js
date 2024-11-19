02609070877d93870f98cf38782de7636914dac2
elenipage <elenipage@icloud.com>
2024-11-19 10:01:33 +0000

    adjust button height

--- src/components/StandardButton.js
+++ src/components/StandardButton.js
@@ -3,7 +3,7 @@ import { Button, Text } from "react-native-paper";
 
 function StandardButton({buttonText, tapFunction}) {
   return <Button mode="contained-tonal"
-  style={{width:"100%", height:50, marginTop:5, marginBottom:5, alignItems: "center", justifyContent:"center"
+  style={{width:"100%", height:55, marginTop:5, marginBottom:5, alignItems: "center", justifyContent:"center"
   }}
   onPress={tapFunction}><Text style={{fontSize:18}}>{buttonText}</Text></Button>;
 }
