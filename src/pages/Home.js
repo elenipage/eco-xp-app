d8397ef3f3ba8956c65d095b26e7fb36447f470c
elenipage <elenipage@icloud.com>
2024-11-19 16:11:49 +0000

    amend merge conflict errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,23 @@ import { useNavigation } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Button } from "react-native-paper";
 import RecyclingTipsCarousel from "../components/RecyclingTips";
-import { Button } from "react-native-paper";
 import { FunFact } from "../components/FunFact";
 
 export function HomeScreen() {
-
   const navigation = useNavigation();
   return (
-
-    <ScrollView style={styles.container}>
-        <Text>Home Screen</Text>
+    <ScrollView>
       <Text>Home Screen</Text>
-      <FunFact/>
-
-      <Button onPress={() => navigation.navigate("Quiz")} mode="contained-tonal">Take a Quiz</Button>
-      <RecyclingTipsCarousel />
+      <View style={styles}>
+        <FunFact />
+        <RecyclingTipsCarousel />
+        <Button
+          onPress={() => navigation.navigate("Quiz")}
+          mode="contained-tonal"
+        >
+          Take a Quiz
+        </Button>
+      </View>
     </ScrollView>
   );
 }
@@ -26,11 +28,11 @@ const insets = useSafeAreaInsets;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      paddingTop: insets.top,
-      paddingBottom: insets.bottom,
-      paddingLeft: insets.left,
-      paddingRight: insets.right,
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: insets.top,
+    paddingBottom: insets.bottom,
+    paddingLeft: insets.left,
+    paddingRight: insets.right,
+  },
+});
