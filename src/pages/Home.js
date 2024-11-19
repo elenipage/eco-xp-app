131ba142c3928ae0dc5257eba90f3c538ff84880
Rob Friskney <rfriskney94@gmail.com>
2024-11-19 14:04:08 +0000

    carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,27 @@ import { Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Button } from "react-native-paper";
-
+import RecyclingTipsCarousel from "../components/RecyclingTips";
 export function HomeScreen() {
   const insets = useSafeAreaInsets;
   const navigation = useNavigation();
   return (
-    <View style={{
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      paddingTop: insets.top,
-      paddingBottom: insets.bottom,
-      paddingLeft: insets.left,
-      paddingRight: insets.right,
-    }}>
-      <Text>Home Screen</Text>
-      <Button onPress={() => navigation.navigate("Quiz")} mode="contained-tonal">Take a Quiz</Button>
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        }}
+      >
+        {/* <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate("Quiz")} mode="contained-tonal">Take a Quiz</Button> */}
+      </View>
+      <RecyclingTipsCarousel />
+    </>
   );
 }
-
