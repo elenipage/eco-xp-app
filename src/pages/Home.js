b01fcb367c0b9af7deaa79aa9cd32dd64e89cd82
Rob Friskney <rfriskney94@gmail.com>
2024-11-25 08:53:41 +0000

    Fixed errors - app should work on mobile now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,8 @@ import { Loader } from "../components/Loader";
 import { useState } from "react";
 
 export function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <ScrollView>
       <BaseLayout>
