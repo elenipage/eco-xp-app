cfcdad353c648f34f912b6b658529fdd5d8bab3a
lsundal1 <126904972+lsundal1@users.noreply.github.com>
2024-11-27 16:16:34 +0000

    styling info.js

--- src/components/PlasticLifeCycle.js
+++ src/components/PlasticLifeCycle.js
@@ -1,24 +1,56 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-  Linking,
-} from "react-native";
-import { Button } from "react-native-paper";
+import { StyleSheet, Text, View, ScrollView, Linking } from "react-native";
+import { Button, useTheme, Surface } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function PlasticLifeCycle() {
+  const { fonts, colors } = useTheme()
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.backgroundColor,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    header: {
+      textAlign: "center",
+      fontSize: 22,
+      letterSpacing: 1.4,
+      textShadowOffset: { width: 1, height: 1 },
+      textShadowRadius: 2,
+      marginBottom: 10,
+    },
+    body: {
+      fontSize: 16,
+      lineHeight: 24,
+      marginBottom: 10,
+    },
+    image: {
+      width: "100%",
+      height: 200,
+      borderRadius: 12,
+      borderWidth: 1,
+      borderColor: "#91E228",
+    }
+  });
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView
-        style={styles.scrollContainer}
         showsVerticalScrollIndicator={false}
       >
-        <View style={styles.card}>
+        <Surface elevation={2}
+              style={{
+                height: "95%",
+                width: "95%",
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 20,
+                padding: 20,
+                margin: 10,
+              }}>
           <Text style={styles.header}>Life Cycle of a Plastic Bottle</Text>
           <Text style={styles.body}>
             The life cycle of a plastic bottle typically begins with the
@@ -42,74 +74,10 @@ export default function PlasticLifeCycle() {
               );
             }}>Learn more</Button>
           
-        </View>
+        </Surface>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: "#B3F9D4",
-  },
-  scrollContainer: {
-    padding: 10,
-  },
-  card: {
-    backgroundColor: "#FFFFFF",
-    borderRadius: 10,
-    padding: 15,
-    marginBottom: 20,
-    borderWidth: 2,
-    borderColor: "#39BCD9",
-    shadowColor: "#1A3151",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-  },
-  header: {
-    textAlign: "center",
-    fontSize: 22,
-    fontFamily: "Roboto_700Bold",
-    color: "#1A3151",
-    marginBottom: 10,
-    letterSpacing: 1.4,
-    textShadowColor: "#6DA99A",
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 2,
-  },
-  body: {
-    fontSize: 16,
-    fontFamily: "Roboto_400Regular",
-    color: "#6DA99A",
-    lineHeight: 24,
-    marginBottom: 10,
-  },
-  image: {
-    width: "100%",
-    height: 200,
-    borderRadius: 12,
-    marginTop: 10,
-    borderWidth: 1,
-    borderColor: "#91E228",
-  },
-  button: {
-    marginTop: 15,
-    backgroundColor: "#39BCD9",
-    color: "#FFFFFF",
-    paddingVertical: 10,
-    borderRadius: 12,
-    alignItems: "center",
-    shadowColor: "#1A3151",
-    // shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-  },
-  buttonText: {
-    color: "#FFFFFF",
-    fontFamily: "Roboto_700Bold",
-    fontSize: 16,
-  },
-});
+
